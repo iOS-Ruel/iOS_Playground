aed65129f92ea13e671e5b04651bfabd5a2db421
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-17 11:15:06 +0900

    💡 SImpleView Project Init

---
+++ SampleProject/SwiftUI_SimpleView/SwiftUI_SimpleView/SwiftUI_SimpleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SimpleViewApp.swift
+//  SwiftUI_SimpleView
+//
+//  Created by Chung Wussup on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SimpleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
