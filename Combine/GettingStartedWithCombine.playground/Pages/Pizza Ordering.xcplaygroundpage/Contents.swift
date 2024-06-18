e0ce24167752d5a59035336b24eec69d110fb72f
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-18 13:21:39 +0900

    💡 Basic Publisher

--- Combine/GettingStartedWithCombine.playground/Pages/Pizza Ordering.xcplaygroundpage/Contents.swift
+++ Combine/GettingStartedWithCombine.playground/Pages/Pizza Ordering.xcplaygroundpage/Contents.swift
@@ -4,7 +4,31 @@ import Foundation
 import Combine
 
 
-let pizzaOrder = 
+let pizzaOrder = Order()
 
 
+let pizzaOrderPublisher = NotificationCenter.default.publisher(for: .didUpdateOrderStatus,
+                                                               object: pizzaOrder)
+
+pizzaOrderPublisher.sink { notification in
+    print(notification)
+    dump(notification)
+}
+
+pizzaOrderPublisher
+    .map { notification in
+        notification.userInfo?["status"] as? OrderStatus ?? OrderStatus.placing
+    }
+    .sink { orderStatus in
+        print("Order sataus [\(orderStatus)]")
+    }
+
+
+
+NotificationCenter.default.post(name: .didUpdateOrderStatus, 
+                                object: pizzaOrder,
+                                userInfo: ["status": OrderStatus.processing])
+
+
+print("Order: \(pizzaOrder.status)")
 //: [Next](@next)
