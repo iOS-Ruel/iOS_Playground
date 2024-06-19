b78eda14ed44a8198236e4e8bd524549257697f2
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-19 09:07:10 +0900

    💡 PlaceDetailViewController 추가

---
+++ SampleProject/CamPlace/CamPlace/PlaceDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PlaceDetailViewController.swift
+//  CamPlace
+//
+//  Created by Chung Wussup on 6/19/24.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+/*
+ "facltNm": 이름
+ "lineIntro": 한줄설명
+ "intro": 설명
+ "allar" : 전체면적
+ "hvofBgnde": 휴무기간 휴무시작일
+ "hvofEnddle": 휴무기간 휴무 종료일
+ "featureNm": 특징
+ "induty": 업종
+ "doNm": 도(ex: 경상북도)
+ "sigunguNm": 시군구(Ex:군위군)
+ "addr1": 주소
+ "addr2": 주소 상세
+ "mapX": 경위
+ "mapY": 위도
+ "direction": 오시는길
+ "tel": 전화
+ "homepage": 홈페이지
+ "firstImageUrl": 대표이미지
+ "animalCmgCl": 애완동물 출입여부
+ "sbrsCl": 부대시설
+ "sbrsEtc": 부대시설 기타
+ 
+ 
+ */
