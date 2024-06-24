c8b19277e036c01c6bc08b196e45a667c16ecc1c
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-24 21:27:19 +0900

    💡 App Naivgation Stack Add

--- SampleProject/WordBrowser_Async/WordBrowser_Async/WordBrowser_AsyncApp.swift
+++ SampleProject/WordBrowser_Async/WordBrowser_Async/WordBrowser_AsyncApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct WordBrowser_AsyncApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
+            
         }
     }
 }
