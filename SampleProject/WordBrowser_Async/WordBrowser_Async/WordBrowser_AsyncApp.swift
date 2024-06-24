a6bf741dae71418153448be49a89681f4e2791c6
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-24 13:20:53 +0900

    💡 WordBrowser_Async Profject init

---
+++ SampleProject/WordBrowser_Async/WordBrowser_Async/WordBrowser_AsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBrowser_AsyncApp.swift
+//  WordBrowser_Async
+//
+//  Created by Chung Wussup on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordBrowser_AsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
