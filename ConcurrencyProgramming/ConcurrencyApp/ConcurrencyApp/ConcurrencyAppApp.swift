e8da392736adbcbcb24df862cea6d317b1486923
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-01 16:24:42 +0900

    💡 Concurrency App Test AsyncAwait

---
+++ ConcurrencyProgramming/ConcurrencyApp/ConcurrencyApp/ConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAppApp.swift
+//  ConcurrencyApp
+//
+//  Created by Chung Wussup on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
