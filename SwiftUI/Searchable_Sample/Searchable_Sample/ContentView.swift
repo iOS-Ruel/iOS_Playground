eb6c176f71cfc866f36db5353e73d22f3a8df8fc
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-03 11:21:32 +0900

    💡 Searchable Test

---
+++ SwiftUI/Searchable_Sample/Searchable_Sample/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Searchable_Sample
+//
+//  Created by Chung Wussup on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var searchText = ""
+    
+    let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
+    
+    var body: some View {
+        NavigationStack {
+            PetListView(animals: petArray)
+        }
+        .searchable(text: $searchText) {
+            ForEach(petArray.filter {
+                $0.hasPrefix(searchText)
+            }, id: \.self) { name in
+                Text(name)   
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct PetListView: View {
+    let animals: [String]
+    
+    var body: some View {
+        List(animals, id: \.self) { animal in
+            Text(animal)
+        }
+    }
+}
---
+++ SwiftUI/Searchable_Sample/Searchable_Sample/Searchable_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Searchable_SampleApp.swift
+//  Searchable_Sample
+//
+//  Created by Chung Wussup on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Searchable_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
