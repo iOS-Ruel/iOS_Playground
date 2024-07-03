4cd7a464f2b12dde983b904c48178ee5ce5138da
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-03 11:45:27 +0900

    💡 Seachable searchCompletion(제안된 검색어 리스트보여주는) 추가

--- SwiftUI/Searchable_Sample/Searchable_Sample/ContentView.swift
+++ SwiftUI/Searchable_Sample/Searchable_Sample/ContentView.swift
@@ -16,17 +16,28 @@ struct ContentView: View {
         NavigationStack {
             PetListView(animals: petArray)
         }
-        .searchable(text: $searchText) {
+        .navigationBarTitleDisplayMode(.automatic)
+        .searchable(text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always),
+                    prompt: "Look for a pet") {
+            
+            //제안하는 키워드가 있을때 사용
+            Text("Singing").searchCompletion("Canary")
+            Text("Croaking").searchCompletion("Frog")
+            Text("Grumpy").searchCompletion("Cat")
+            
+            Divider()
             //SearchText 가 비어있을 경우, hasPrefix는 true를 리턴
             //배열의 모든 요소가 출력됨
             ForEach(petArray.filter {
                 $0.hasPrefix(searchText)
             }, id: \.self) { name in
-                Text(name)   
+                Text(name)
             }
         }
     }
 }
+
 
 #Preview {
     ContentView()
@@ -39,5 +50,6 @@ struct PetListView: View {
         List(animals, id: \.self) { animal in
             Text(animal)
         }
+        .listStyle(.plain)
     }
 }
