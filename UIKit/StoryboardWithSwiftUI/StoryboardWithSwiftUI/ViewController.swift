79ba63d8bc02eecd489e625c04597695db906df6
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-08 13:22:48 +0900

    💡 UIKit with SwiftUI -> StoryBoard에서 Hosting Controller 사용하여 SwiftUI View 결합

---
+++ UIKit/StoryboardWithSwiftUI/StoryboardWithSwiftUI/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  StoryboardWithSwiftUI
+//
+//  Created by Chung Wussup on 7/8/24.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBSegueAction func openSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: SwiftUIView(name: "Ruel"))
+    }
+    
+}
+
