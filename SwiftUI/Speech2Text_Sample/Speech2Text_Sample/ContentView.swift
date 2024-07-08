ff233fe30eb21bcd89060ed4901be8e0d9ac112b
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-08 09:49:48 +0900

    💡 SpeechToText(STT) Test with SwiftUI

---
+++ SwiftUI/Speech2Text_Sample/Speech2Text_Sample/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  Speech2Text_Sample
+//
+//  Created by Chung Wussup on 7/8/24.
+//
+
+import SwiftUI
+import Speech
+
+struct ContentView: View {
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko-KR"))
+    
+    @State var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    @State var recognitionTask: SFSpeechRecognitionTask?
+    
+    @State var message = ""
+    @State var buttonStatus = true
+    
+    var body: some View {
+        VStack {
+            TextEditor(text: $message)
+                .frame(width: 300, height: 400)
+            Button(buttonStatus ? "Start recording" : "Stop Recording") {
+                buttonStatus.toggle()
+                
+                if buttonStatus {
+                    stopRecording()
+                } else {
+                    startRecording()
+                }
+            }
+        }
+        .padding()
+    }
+    
+    
+    func startRecording() {
+        message = "start recording"
+        let node = audioEngine.inputNode
+        //저장공간
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        recognitionRequest?.shouldReportPartialResults = true
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffet, _) in
+            recognitionRequest?.append(buffet)
+        }
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let recognizeMe = SFSpeechRecognizer() else {
+            return
+        }
+        if !recognizeMe.isAvailable { return
+        }
+        
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest ?? SFSpeechAudioBufferRecognitionRequest.init(), resultHandler: { result, error in
+            if let result = result {
+                let transcrinbedString = result.bestTranscription.formattedString
+                message = transcrinbedString
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+    
+    func stopRecording() {
+        audioEngine.stop()
+        recognitionTask?.cancel()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        recognitionRequest?.endAudio()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Speech2Text_Sample/Speech2Text_Sample/Speech2Text_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech2Text_SampleApp.swift
+//  Speech2Text_Sample
+//
+//  Created by Chung Wussup on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Speech2Text_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
