6d70ea206fd8e85554f7b3e4f970f376c0631860
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-09 14:23:00 +0900

    💡 Natural Language -> 언어감지 Sample

---
+++ SwiftUI/NaturalLanguage_SwiftUI_Sample/NaturalLanguage_SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  NaturalLanguage_SwiftUI_Sample
+//
+//  Created by Chung Wussup on 7/9/24.
+//
+
+import SwiftUI
+import NaturalLanguage
+
+struct ContentView: View {
+    @State var language = ""
+    @State var message = ""
+    
+    
+    var body: some View {
+        VStack {
+            TextField("Type foreign text here", text: $language)
+                .autocorrectionDisabled()
+                .padding()
+            Text("Language = \(message)")
+                .padding()
+            Button {
+                if let prediction = predictLanguage(text: language){
+                    message = prediction
+                }
+            } label: {
+                Text("Identify Language")
+            }
+        }
+        
+        .padding()
+    }
+    
+    
+    func predictLanguage(text: String) -> String? {
+        let locale = Locale(identifier: "ko")
+        let recognizer = NLLanguageRecognizer()
+        recognizer.processString(text)
+        
+        guard let language = recognizer.dominantLanguage else {
+            return "Unknown language"
+        }
+        
+        return locale.localizedString(forIdentifier: language.rawValue)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/NaturalLanguage_SwiftUI_Sample/NaturalLanguage_SwiftUI_Sample/NaturalLanguage_SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalLanguage_SwiftUI_SampleApp.swift
+//  NaturalLanguage_SwiftUI_Sample
+//
+//  Created by Chung Wussup on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NaturalLanguage_SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
