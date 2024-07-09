ee1117201a509ff1e325f7c0c89e753b850ef1ec
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-07-09 09:38:46 +0900

    💡 Vision Framework - FacialRecorgnition 사진으로 사람 얼굴 확인하기

---
+++ SwiftUI/FacialRecognition_SwiftUI_Sample/FacialRecognition_SwiftUI_Sample/FacialRecognition_SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognition_SwiftUI_SampleApp.swift
+//  FacialRecognition_SwiftUI_Sample
+//
+//  Created by Chung Wussup on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognition_SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
