ebe53aa4044eb92717606eb0dd9ca3b167361d56
Ruel <c0407h@gmail.com>
2024-07-15 11:23:11 +0900

    Feat: greet path 추가 ( parameter : name )
    파라미터 전달 라우트 greet 추가

--- VAPOR/helloWolrd/Sources/App/routes.swift
+++ VAPOR/helloWolrd/Sources/App/routes.swift
@@ -8,4 +8,12 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("greet", ":name") { req async -> String in
+        guard let guest = req.parameters.get("name") else {
+            return "Hi!"
+        }
+        
+        return "Hi, \(guest), grettings! Thanks for visiting us."
+    }
 }
