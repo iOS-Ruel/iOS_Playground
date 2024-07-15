bb67ea02259ae745c88d1d53659d6ef013ef3250
Ruel <c0407h@gmail.com>
2024-07-15 11:47:50 +0900

    Feat: student path 추가
    Paramter가 있을때와 없을때 두개의 Path 추가
    Paramter 따른 return 값 변경

--- VAPOR/helloWolrd/Sources/App/routes.swift
+++ VAPOR/helloWolrd/Sources/App/routes.swift
@@ -4,7 +4,7 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
-
+    
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
@@ -16,4 +16,32 @@ func routes(_ app: Application) throws {
         
         return "Hi, \(guest), grettings! Thanks for visiting us."
     }
+    
+    app.get("student") { req async throws -> String in
+        throw Abort(.badRequest)
+    }
+    
+    app.get("student", ":name") { req async throws -> String in
+        guard let studentName = req.parameters.get("name") else {
+            throw Abort(.badRequest)
+        }
+        
+        let studentRecords = [
+            "Peter" : 3.42,
+            "Thomas" : 2.98,
+            "Jane" : 3.91,
+            "Ryan" : 4.00,
+            "Kyle" : 4.00
+        ]
+        
+        if let gpa = studentRecords[studentName] {
+            return "The student \(studentName)'s GPA is \(gpa)"
+        } else {
+            return "The student's record can't be found!"
+        }
+        
+        
+        
+    }
+    
 }
