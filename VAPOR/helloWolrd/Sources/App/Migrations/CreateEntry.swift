f9dd11a466bf82c631462224e259b7b96768b83a
Ruel <c0407h@gmail.com>
2024-07-16 10:13:45 +0900

    Feat: 모델 데이터베이스 마이그레이션 코드 추가

---
+++ VAPOR/helloWolrd/Sources/App/Migrations/CreateEntry.swift
@@ -0,0 +1,25 @@
+//
+//  CreateEntry.swift
+//
+//
+//  Created by Chung Wussup on 7/16/24.
+//
+
+import Fluent
+
+struct CreateEntry: Migration {
+    func prepare(on database: any FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+        return database.schema("entries")
+            .id()
+            .field("title", .string, .required)
+            .field("content", .string, .required)
+            .create()
+    }
+    
+    func revert(on database: any FluentKit.Database) -> NIOCore.EventLoopFuture<Void> {
+        return database.schema("entries")
+            .delete()
+    }
+    
+    
+}
