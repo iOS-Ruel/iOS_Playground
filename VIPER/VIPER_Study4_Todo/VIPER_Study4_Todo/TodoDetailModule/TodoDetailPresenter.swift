88164efdc7c641dbf6ca959a18238916943c13a0
Ruel <c0407h@gmail.com>
2024-07-17 18:58:49 +0900

    Feat: TodoDetailPresenter추가

---
+++ VIPER/VIPER_Study4_Todo/VIPER_Study4_Todo/TodoDetailModule/TodoDetailPresenter.swift
@@ -0,0 +1,44 @@
+//
+//  TodoDetailPresenter.swift
+//  VIPER_Study4_Todo
+//
+//  Created by Chung Wussup on 7/17/24.
+//
+
+import Foundation
+
+class TodoDetailPresenter: TodoDetailPresenterProtocol {
+    weak var view: TodoDetailViewProtocol?
+    var interactor: TodoDetailInteractorInputProtocol?
+    var router: TodoListRouter?
+    
+    func viewDidLoad() {
+        if let todoItem = interactor?.todoItem {
+            view?.showToDo(todoItem)
+        }
+    }
+    
+    func editTodo(title: String, content: String) {
+        interactor?.editTodo(title: title, content: content)
+    }
+    
+    func deleteTodo() {
+        interactor?.deleteTodo()
+    }
+    
+    
+}
+
+extension TodoDetailPresenter: TodoDetailInteractorOutputProtocol {
+    func didDeleteTodo() {
+        if let view = view {
+            //router?.navigateBackToListViewController(from: view)
+        }
+    }
+    
+    func didEditTodo(_ todo: TodoItem) {
+        view?.showToDo(todo)
+    }
+    
+    
+}
