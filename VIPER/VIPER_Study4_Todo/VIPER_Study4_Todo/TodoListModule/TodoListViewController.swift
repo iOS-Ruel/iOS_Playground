67450b6cb607c0d94234bb2ec3a1af646ad6353c
Ruel <c0407h@gmail.com>
2024-07-17 16:07:11 +0900

    Feat: TodoListViewController 추가
    - 예시에서 TodoListViewController는 UITableView 타입으로 예제와 똑같이 진행
    - TodoListViewController에서 TodoListPresenterProtocol의 메서드들을 사용
    - TodoListViewController에서 TodoListViewProtocol를 채택하여 TodoListViewProtocol 메서드 구현

---
+++ VIPER/VIPER_Study4_Todo/VIPER_Study4_Todo/TodoListModule/TodoListViewController.swift
@@ -0,0 +1,96 @@
+//
+//  TodoListViewController.swift
+//  VIPER_Study4_Todo
+//
+//  Created by Chung Wussup on 7/17/24.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var presenter: TodoListPresenterProtocol?
+    var todos: [TodoItem] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        presenter?.viewWillAppear()
+    }
+    
+    private func setupView() {
+        tableView.tableFooterView = UIView()
+    }
+    
+}
+
+extension TodoListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        cell.detailTextLabel?.text = todo.content
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let todo = todos[indexPath.row]
+        presenter?.showTodoDetail(todo)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let todoItem = todos[indexPath.row]
+            presenter?.removeTodo(todoItem)
+        }
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "Add Todo Item",
+                                                message: "Enter title and content",
+                                                preferredStyle: .alert)
+        
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addTextField(configurationHandler: nil)
+        
+        alertController.addAction(UIAlertAction(title: "Confirm", style: .destructive, handler: { [weak self] _ in
+            let titleText = alertController.textFields![0].text ?? ""
+            let contentText = alertController .textFields![1].text ?? ""
+            guard !titleText.isEmpty else { return }
+            let todoItem = TodoItem(title: titleText, content: contentText)
+            self?.presenter?.addTodo(todoItem)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true)
+    }
+    
+}
+
+
+extension TodoListViewController: TodoListViewProtocol {
+    func showTodos(_ todos: [TodoItem]) {
+        self.todos = todos
+    }
+    
+    func showErrorMessage(_ message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
