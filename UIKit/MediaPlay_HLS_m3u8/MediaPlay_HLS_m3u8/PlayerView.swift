b2c87129b4a9ab64f5de867a1da4161574f1e85a
Ruel <c0407h@gmail.com>
2024-07-21 16:20:53 +0900

    💡 PlayerView Sample - 영상 재생시간 표시 - 영상 재생 슬라이더 표시

--- UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/PlayerView.swift
+++ UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/PlayerView.swift
@@ -13,43 +13,36 @@ class PlayerView: UIView {
     private var playTimeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "00:00"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .white
         return label
     }()
     
     private var totalPlayTimeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "00:00"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .white
         return label
     }()
     
-    private var playerSider: UISlider = {
-        let slider = UISlider()
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        return slider
+    private var playerSiderView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 2
+        return view
     }()
     
-    
-    
-    override class var layerClass: AnyClass {
-        AVPlayerLayer.self
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        
-    }
-    
-    func setupPlayerView() {
-        self.backgroundColor = .black
-        
-        
-    }
-    
+    private var playerPlaySiderView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        view.layer.cornerRadius = 2
+        return view
+    }()
     
     var playerLayer: AVPlayerLayer {
         layer as! AVPlayerLayer
@@ -72,11 +65,78 @@ class PlayerView: UIView {
         return player?.currentItem?.duration.seconds ?? 0
     }
     
-    var duration : CMTime {
+    var duration: CMTime {
         return player?.currentItem?.duration ?? CMTime(seconds: 1, preferredTimescale: 1)
     }
+    
     var currentItem: AVPlayerItem? {
         return player?.currentItem
     }
     
+    private var playerPlaySiderWidthConstraint: NSLayoutConstraint!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupPlayerView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupPlayerView()
+    }
+    
+    override class var layerClass: AnyClass {
+        AVPlayerLayer.self
+    }
+    
+    
+    func setupPlayerView() {
+        self.backgroundColor = .black
+        [playerSiderView, playTimeLabel, totalPlayTimeLabel].forEach {
+            addSubview($0)
+        }
+        
+        playerSiderView.addSubview(playerPlaySiderView)
+        
+        playerPlaySiderWidthConstraint = playerPlaySiderView.widthAnchor.constraint(equalToConstant: 0)
+        
+        NSLayoutConstraint.activate([
+            playerSiderView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            playerSiderView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            playerSiderView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            playerSiderView.heightAnchor.constraint(equalToConstant: 5),
+            
+            totalPlayTimeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            totalPlayTimeLabel.bottomAnchor.constraint(equalTo: playerSiderView.topAnchor, constant: -10),
+
+            playTimeLabel.trailingAnchor.constraint(equalTo: totalPlayTimeLabel.leadingAnchor),
+            playTimeLabel.bottomAnchor.constraint(equalTo: playerSiderView.topAnchor, constant: -10),
+            playTimeLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor,constant: 16),
+            
+            playerPlaySiderView.leadingAnchor.constraint(equalTo: playerSiderView.leadingAnchor),
+            playerPlaySiderView.topAnchor.constraint(equalTo: playerSiderView.topAnchor),
+            playerPlaySiderView.bottomAnchor.constraint(equalTo: playerSiderView.bottomAnchor),
+            playerPlaySiderWidthConstraint
+        ])
+    }
+    
+    
+    func updatePlayTimeLabel(with time: String) {
+        playTimeLabel.text = time
+    }
+    
+    func updateTotalPlayTimeLabel(with time: String) {
+        totalPlayTimeLabel.text = time
+    }
+    
+    func updateSlider(currentTime: Double, totalTime: Double) {
+        guard !currentTime.isNaN, !totalTime.isNaN, totalTime > 0 else {
+            playerPlaySiderWidthConstraint.constant = 0
+            return
+        }
+        
+        let progress = currentTime / totalTime
+        playerPlaySiderWidthConstraint.constant = CGFloat(progress) * playerSiderView.frame.width
+        layoutIfNeeded()
+    }
 }
--- UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/ViewController.swift
+++ UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MediaPlay_HLS_m3u8
 //
-//  Created by Chung Wussup 
+//  Created by Chung Wussup
 //
 
 import UIKit
@@ -12,27 +12,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerView: PlayerView!
     @IBOutlet weak var urlTextField: UITextField!
     
+    
+    var timeObserver: Any?
+    let player = AVPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func searchTouch(_ sender: Any) {
-        guard let urlString = urlTextField.text else {
-            return
+        setupPlayerView()
+        Task {
+            await setupPlayer(urlString: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")
         }
-        setupPlayer(urlString: urlString)
+    }
+    
+    func setupPlayerView() {
+        playerView.player = player
+        timeObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.1, preferredTimescale: 10), queue: DispatchQueue.main) { [weak self] time in
+            self?.updateCurrentTime(time: time)
+        }
+    }
+    
+    @IBAction func searchTouch(_ sender: Any) {
+        Task { @MainActor in
+            guard let urlString = urlTextField.text else {
+                return
+            }
+            await setupPlayer(urlString: urlString)
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
     
-    private func setupPlayer(urlString: String) {
+    private func setupPlayer(urlString: String) async {
         if let url = URL(string: urlString) {
             let playerItem = AVPlayerItem(url: url)
             playerItem.preferredForwardBufferDuration = TimeInterval(1.0)
-            playerView.player = AVPlayer(playerItem: playerItem)
+            player.replaceCurrentItem(with: playerItem)
+            
+            // 전체 시간을 설정
+            let assetDuration = try? await playerItem.asset.load(.duration)
+            let totalSeconds = CMTimeGetSeconds(assetDuration ?? CMTime())
+            
+            if !totalSeconds.isNaN {
+                let totalSecondsInt = Int(totalSeconds)
+                let seconds = totalSecondsInt % 60
+                let minutes = totalSecondsInt / 60
+                let totalTimeString = String(format: "%01d:%02d", minutes, seconds)
+                playerView.updateTotalPlayTimeLabel(with: " / \(totalTimeString)")
+            }
         }
     }
     
@@ -44,7 +72,20 @@ class ViewController: UIViewController {
         playerView.player?.pause()
     }
     
-
+    
+    func updateCurrentTime(time: CMTime) {
+        let currentSeconds = Int(time.seconds)
+        let totalSeconds = playerView.totalDurationTime
+        
+        let seconds = currentSeconds % 60
+        let minutes = currentSeconds / 60
+        
+        let timeFormatString  = String(format: "%01d:%02d", minutes, seconds)
+        
+        playerView.updatePlayTimeLabel(with: timeFormatString)
+        playerView.updateSlider(currentTime: time.seconds, totalTime: totalSeconds)
+    }
+    
 }
 
 
