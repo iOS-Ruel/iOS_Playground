a17db6c6b27afe7d5040fb6b78cc00570bc06d36
Ruel <c0407h@gmail.com>
2024-07-22 22:37:53 +0900

    💡 PlayerView로 기능 이전

--- UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/PlayerView.swift
+++ UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/PlayerView.swift
@@ -8,7 +8,20 @@
 import Foundation
 import AVKit
 
-class PlayerView: UIView {
+protocol PlayerViewDelegate: AnyObject {
+    func configurePlayer(with url: URL) async
+}
+
+class PlayerView: UIView, PlayerViewDelegate {
+    
+    private lazy var controllButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.addTarget(self, action: #selector(handleControlButton), for: .touchUpInside)
+        button.isHidden = true
+        return button
+    }()
     
     private let playTimeLabel: UILabel = {
         let label = UILabel()
@@ -47,25 +60,19 @@ class PlayerView: UIView {
         return view
     }()
     
+    private let player = AVPlayer()
+    private var timeObserver: Any?
+    
     var playerLayer: AVPlayerLayer {
         return layer as! AVPlayerLayer
     }
     
-    var player: AVPlayer? {
-        get {
-            return playerLayer.player
-        }
-        set {
-            playerLayer.player = newValue
-        }
-    }
-    
     var currentTime: Double {
-        return player?.currentItem?.currentTime().seconds ?? 0
+        return player.currentItem?.currentTime().seconds ?? 0
     }
     
     var totalDurationTime: Double {
-        return player?.currentItem?.duration.seconds ?? 0
+        return player.currentItem?.duration.seconds ?? 0
     }
     
     private var playerPlaySiderWidthConstraint: NSLayoutConstraint!
@@ -86,7 +93,7 @@ class PlayerView: UIView {
     
     private func setupPlayerView() {
         backgroundColor = .black
-        [playerSliderView, playTimeLabel, totalPlayTimeLabel].forEach { addSubview($0) }
+        [playerSliderView, playTimeLabel, totalPlayTimeLabel, controllButton].forEach { addSubview($0) }
         
         playerSliderView.addSubview(playerPlaySliderView)
         
@@ -103,13 +110,35 @@ class PlayerView: UIView {
             
             playTimeLabel.trailingAnchor.constraint(equalTo: totalPlayTimeLabel.leadingAnchor, constant: -8),
             playTimeLabel.bottomAnchor.constraint(equalTo: playerSliderView.topAnchor, constant: -10),
-            playTimeLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor,constant: 16),
+            playTimeLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 16),
             
             playerPlaySliderView.leadingAnchor.constraint(equalTo: playerSliderView.leadingAnchor),
             playerPlaySliderView.topAnchor.constraint(equalTo: playerSliderView.topAnchor),
             playerPlaySliderView.bottomAnchor.constraint(equalTo: playerSliderView.bottomAnchor),
-            playerPlaySiderWidthConstraint
+            playerPlaySiderWidthConstraint,
+            
+            controllButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            controllButton.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
+        
+        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapPlayerView))
+        self.addGestureRecognizer(singleTapGesture)
+        
+        let leftDoubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleLeftDoubleTap))
+        leftDoubleTapGesture.numberOfTapsRequired = 2
+        self.addGestureRecognizer(leftDoubleTapGesture)
+        
+        let rightDoubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleRightDoubleTap))
+        rightDoubleTapGesture.numberOfTapsRequired = 2
+        self.addGestureRecognizer(rightDoubleTapGesture)
+        
+        singleTapGesture.require(toFail: leftDoubleTapGesture)
+        singleTapGesture.require(toFail: rightDoubleTapGesture)
+        
+        playerLayer.player = player
+        addPeriodicTimeObserver()
+        
+        player.addObserver(self, forKeyPath: "timeControlStatus", options: [.old, .new], context: nil)
     }
     
     func updatePlayTimeLabel(with time: String) {
@@ -138,13 +167,142 @@ class PlayerView: UIView {
         let progress = min(max(location.x / totalWidth, 0), 1)
         
         switch gesture.state {
-        case .began, .changed :
+        case .began, .changed:
             updateSlider(currentTime: Double(progress) * totalDurationTime, totalTime: totalDurationTime)
         case .ended:
             let seekTime = CMTime(seconds: Double(progress) * totalDurationTime, preferredTimescale: 600)
-            player?.seek(to: seekTime, toleranceBefore: .zero, toleranceAfter: .zero)
+            player.seek(to: seekTime, toleranceBefore: .zero, toleranceAfter: .zero)
         default:
             break
         }
     }
+    
+    @objc private func didTapPlayerView(_ gesture: UITapGestureRecognizer) {
+        controllButtonSetup()
+    }
+    
+    private func controllButtonSetup() {
+        controllButton.isHidden.toggle()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.controllButton.isHidden = true
+        }
+    }
+    
+    @objc private func handleControlButton() {
+        
+        if player.timeControlStatus == .paused {
+            play()
+        } else {
+            pause()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            self?.controllButton.isHidden = false
+        }
+        
+    }
+    
+    @objc private func handleLeftDoubleTap(_ gesture: UITapGestureRecognizer) {
+        let tapLocation = gesture.location(in: self)
+        if tapLocation.x < self.bounds.width / 2 {
+            seekBackward()
+        }
+    }
+    
+    @objc private func handleRightDoubleTap(_ gesture: UITapGestureRecognizer) {
+        let tapLocation = gesture.location(in: self)
+        if tapLocation.x > self.bounds.width / 2 {
+            seekForward()
+        }
+    }
+    
+    private func seekBackward() {
+        let newTime = max(currentTime - 10, 0)
+        let seekTime = CMTime(seconds: newTime, preferredTimescale: 600)
+        player.seek(to: seekTime, toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+    
+    private func seekForward() {
+        let newTime = min(currentTime + 10, totalDurationTime)
+        let seekTime = CMTime(seconds: newTime, preferredTimescale: 600)
+        player.seek(to: seekTime, toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+    
+    private func addPeriodicTimeObserver() {
+        timeObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.1, preferredTimescale: 10), queue: .main) { [weak self] time in
+            self?.updateCurrentTime(time: time)
+        }
+    }
+    
+    private func updateCurrentTime(time: CMTime) {
+        let currentSeconds = Int(time.seconds)
+        let totalSeconds = totalDurationTime
+        
+        let seconds = currentSeconds % 60
+        let minutes = currentSeconds / 60
+        
+        let timeFormatString = String(format: "%01d:%02d", minutes, seconds)
+        DispatchQueue.main.async {
+            self.updatePlayTimeLabel(with: timeFormatString)
+            self.updateSlider(currentTime: time.seconds, totalTime: totalSeconds)
+        }
+    }
+    
+    func configurePlayer(with url: URL) async {
+        let playerItem = AVPlayerItem(url: url)
+        playerItem.preferredForwardBufferDuration = TimeInterval(1.0)
+        
+        DispatchQueue.main.async {
+            self.player.replaceCurrentItem(with: playerItem)
+            self.controllButton.isHidden = false
+        }
+        
+        do {
+            let assetDuration = try await playerItem.asset.load(.duration)
+            let totalSeconds = CMTimeGetSeconds(assetDuration)
+            
+            if !totalSeconds.isNaN {
+                let totalSecondsInt = Int(totalSeconds)
+                let seconds = totalSecondsInt % 60
+                let minutes = totalSecondsInt / 60
+                let totalTimeString = String(format: "%01d:%02d", minutes, seconds)
+                DispatchQueue.main.async {
+                    self.updateTotalPlayTimeLabel(with: "/ \(totalTimeString)")
+                }
+            }
+        } catch {
+            
+        }
+    }
+    
+    func play() {
+        player.play()
+        controllButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+    }
+    
+    func pause() {
+        player.pause()
+        controllButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+    }
+    
+    func controllButtonHidden() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            self?.controllButton.isHidden = false
+        }
+    }
+    
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "timeControlStatus" {
+            DispatchQueue.main.async {
+                if self.player.timeControlStatus == .playing {
+                    self.controllButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+                } else {
+                    self.controllButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+                }
+            }
+        }
+    }
+    
 }
--- UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/ViewController.swift
+++ UIKit/MediaPlay_HLS_m3u8/MediaPlay_HLS_m3u8/ViewController.swift
@@ -12,29 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerView: PlayerView!
     @IBOutlet weak var urlTextField: UITextField!
     
-    var timeObserver: Any?
-    let player = AVPlayer()
+    private var playerViewDelegate: PlayerViewDelegate? {
+        return playerView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupPlayerView()
-    }
-    
-    func setupPlayerView() {
-        playerView.player = player
-        
-        timeObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.1, preferredTimescale: 10), queue: .main) { [weak self] time in
-            self?.updateCurrentTime(time: time)
-        }
     }
     
     @IBAction func searchTouch(_ sender: Any) {
         Task { @MainActor in
-            guard let urlString = urlTextField.text, !urlString.isEmpty else {
+            guard let urlString = urlTextField.text, !urlString.isEmpty, 
+                    let url = URL(string: urlString) else {
                 showAlert(message: "Please enter a valid URL")
                 return
             }
-            await setupPlayer(urlString: urlString)
+            await playerViewDelegate?.configurePlayer(with: url)
         }
     }
     
@@ -42,59 +35,6 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
-    private func setupPlayer(urlString: String) async {
-        guard let url = URL(string: urlString) else {
-            showAlert(message: "Invalid URL")
-            return
-        }
-        
-        let playerItem = AVPlayerItem(url: url)
-        playerItem.preferredForwardBufferDuration = TimeInterval(1.0)
-        
-        DispatchQueue.main.async {
-            self.player.replaceCurrentItem(with: playerItem)
-        }
-        
-        do {
-            let assetDuration = try await playerItem.asset.load(.duration)
-            let totalSeconds = CMTimeGetSeconds(assetDuration)
-            
-            if !totalSeconds.isNaN {
-                let totalSecondsInt = Int(totalSeconds)
-                let seconds = totalSecondsInt % 60
-                let minutes = totalSecondsInt / 60
-                let totalTimeString = String(format: "%01d:%02d", minutes, seconds)
-                DispatchQueue.main.async {
-                    self.playerView.updateTotalPlayTimeLabel(with: "/ \(totalTimeString)")
-                }
-            }
-        } catch {
-            showAlert(message: "Failed to load media duration")
-        }
-    }
-    
-    @IBAction func playButtonTouch(_ sender: Any) {
-        playerView.player?.play()
-    }
-    
-    @IBAction func stopButtonTouch(_ sender: Any) {
-        playerView.player?.pause()
-    }
-    
-    func updateCurrentTime(time: CMTime) {
-        let currentSeconds = Int(time.seconds)
-        let totalSeconds = playerView.totalDurationTime
-        
-        let seconds = currentSeconds % 60
-        let minutes = currentSeconds / 60
-        
-        let timeFormatString = String(format: "%01d:%02d", minutes, seconds)
-        DispatchQueue.main.async {
-            self.playerView.updatePlayTimeLabel(with: timeFormatString)
-            
-            self.playerView.updateSlider(currentTime: time.seconds, totalTime: totalSeconds)
-        }
-    }
     
     private func showAlert(message: String) {
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
