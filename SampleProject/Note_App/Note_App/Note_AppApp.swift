21f81904ba7127425bc844f5f26753ce50a73091
Ruel <c0407h@gmail.com>
2024-07-22 10:10:26 +0900

    Init: NoteApp Init
    - SwiftUI로 구현한 Note App

---
+++ SampleProject/Note_App/Note_App/Note_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_AppApp.swift
+//  Note_App
+//
+//  Created by Chung Wussup on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Note_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
