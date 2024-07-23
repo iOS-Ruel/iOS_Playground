b5870feb749064b565a67dc76d8d7b2a3e8d74c2
Ruel <c0407h@gmail.com>
2024-07-23 10:27:31 +0900

    Feat: AuthViewModel Auth 기능 구현
    - signIn
    - signOut
    - signUp
    Auth 관련 기능 구현

---
+++ SampleProject/Note_App/Note_App/AuthViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  AuthViewModel.swift
+//  Note_App
+//
+//  Created by Chung Wussup on 7/23/24.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+final class AuthViewModel: ObservableObject {
+    @Published var user: User?
+    
+    func listenToAuthState() {
+        Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            guard let self = self else {
+                return
+            }
+            
+            self.user = user 
+            
+        }
+    }
+    
+    func signIn(emailAddress: String, password: String) {
+        Auth.auth().signIn(withEmail: emailAddress, password: password) { result, error in
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+                return
+            }
+        }
+    }
+    
+    func signUp(emailAddress: String, password: String) {
+        Auth.auth().createUser(withEmail: emailAddress, password: password) { result, error in
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+                return
+            }
+            dump(result)
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError.localizedDescription)
+        }
+        
+        
+    }
+}
