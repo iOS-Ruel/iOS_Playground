e8302d54c752b50d7aac4e468fef2aec68235935
Ruel <c0407h@gmail.com>
2024-07-24 10:48:16 +0900

    Refactor: WindowGroup 시작점 변경
    - 로그인여부에 따라 탭뷰 처리

--- SampleProject/Socially/Socially/SociallyApp.swift
+++ SampleProject/Socially/Socially/SociallyApp.swift
@@ -25,22 +25,22 @@ struct SociallyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            //            Group {
-            //                if authModel.user == nil {
-            //                    SignUpView()
-            //                } else {
-            //                    FeedView()
-            //                        .environmentObject(PostViewModel())
-            //                }
-            //            }
-            //            .environmentObject(authModel)
-            //            .onAppear {
-            //                authModel.listenToAuthState()
-            //            }
-//            ProfileView()
-//                .environmentObject(authModel)
-            FeedView()
-                .environmentObject(PostViewModel())
+            TabView {
+                if authModel.user != nil {
+                    FeedView()
+                        .tabItem {
+                            Image(systemName: "text.bubble")
+                            Text("Feed")
+                        }
+                }
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Account")
+                    }
+            }
+            .environmentObject(authModel)
+            .environmentObject(PostViewModel())
         }
     }
 }
