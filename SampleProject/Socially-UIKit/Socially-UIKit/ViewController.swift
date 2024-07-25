ae875fa47761f7b17a66ac689966962d14ef8503
Ruel <c0407h@gmail.com>
2024-07-25 09:41:11 +0900

    Init: Socially - UIKit project init

---
+++ SampleProject/Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socially-UIKit
+//
+//  Created by Chung Wussup on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
